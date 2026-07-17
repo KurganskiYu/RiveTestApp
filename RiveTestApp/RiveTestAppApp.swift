ff89fc4baa6e96b58bbf1d2a78021c41857e4002
Yuri <jurasiq@gmail.com>
2026-07-17 21:55:35 +0100

    Initial Commit

---
+++ RiveTestApp/RiveTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RiveTestAppApp.swift
+//  RiveTestApp
+//
+//  Created by Yuri Kurganski on 17.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct RiveTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
